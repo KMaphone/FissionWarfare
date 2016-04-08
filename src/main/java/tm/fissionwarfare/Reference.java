fa9fca60219f0a7618457b21f61c04d2a3a7c2f5
MegabyteMage <kryto627@yahoo.com>
2016-04-07 22:23:53 -0400

    Fixed proxy

--- src/main/java/tm/fissionwarfare/Reference.java
+++ src/main/java/tm/fissionwarfare/Reference.java
@@ -5,7 +5,7 @@ public class Reference {
 	public static final String MOD_ID = "FissionWarfare";
 	public static final String MOD_NAME = "Fission Warfare";
 	public static final String VERSION = "1.7.10-1.0";
-	public static final String CLIENT_PROXY_CLASS = "tm.fw.proxy.ClientProxy";
-	public static final String SERVER_PROXY_CLASS = "tm.fw.proxy.ServerProxy";
-	public static final String GUI_FACTORY_CLASS = "tm.fw.gui.GuiFactory";
+	public static final String CLIENT_PROXY_CLASS = "tm.fissionwarfare.proxy.ClientProxy";
+	public static final String SERVER_PROXY_CLASS = "tm.fissionwarfare.proxy.ServerProxy";
+	public static final String GUI_FACTORY_CLASS = "tm.fissionwarfare.gui.GuiFactory";
 }
