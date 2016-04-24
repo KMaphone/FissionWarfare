2f70d34c549a8b5eaee3a2e8e035abbd58d00cc8
MrFlakes <flakeybisquik@hotmail.com>
2016-04-24 18:42:18 -0400

    lol why is he in on the timeline
    :0

--- src/main/java/tm/fissionwarfare/init/InitBlocks.java
+++ src/main/java/tm/fissionwarfare/init/InitBlocks.java
@@ -2,7 +2,9 @@ package tm.fissionwarfare.init;
 
 import cofh.thermalfoundation.item.TFItems;
 import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
 import net.minecraft.item.ItemStack;
+import tm.fissionwarfare.block.BlockBase;
 import tm.fissionwarfare.block.BlockConcrete;
 import tm.fissionwarfare.block.BlockExplosive;
 import tm.fissionwarfare.block.BlockOreBase;
@@ -14,12 +16,15 @@ public class InitBlocks {
 	
 	public static Block basicExplosive;
 	public static Block concrete;
+	public static Block limestone;
 	
 	public static void init() {
 			
 		quartz_ore = new BlockOreBase("quartz").setDroppedItem(new ItemStack(InitItems.quartz), 1, 2, 1, 2);
 		sulfur_ore = new BlockOreBase("sulfur").setDroppedItem(TFItems.dustSulfur, 1, 2, 2, 4);		
 		uranium_ore = new BlockOreBase("uranium").setLightLevel(0.3F);
+		
+		limestone = new BlockBase("limestone" , Material.rock, 1, 3.0F, 3.0F, Block.soundTypeStone);
 		
 		basicExplosive = new BlockExplosive("basic", new BasicExplosion(), 80);
 		concrete = new BlockConcrete();		
