95eb12167e61f68b175e8e8462ad620a002ae2db
Grab Chips <thesteampunksteve@gmail.com>
2016-04-24 18:39:43 -0400

    Particles!

--- src/main/java/tm/fissionwarfare/block/BlockConcrete.java
+++ src/main/java/tm/fissionwarfare/block/BlockConcrete.java
@@ -1,6 +1,7 @@
 package tm.fissionwarfare.block;
 
 import java.util.List;
+import java.util.Random;
 
 import cpw.mods.fml.common.registry.GameRegistry;
 import cpw.mods.fml.relauncher.Side;
@@ -9,6 +10,8 @@ import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.IIcon;
@@ -16,6 +19,7 @@ import net.minecraft.world.Explosion;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 import tm.fissionwarfare.Reference;
+import tm.fissionwarfare.init.InitBlocks;
 import tm.fissionwarfare.init.InitTabs;
 import tm.fissionwarfare.item.ItemBlockConcrete;
 import tm.fissionwarfare.item.ItemBlockMeta;
@@ -30,10 +34,15 @@ public class BlockConcrete extends BlockMetaBase implements IConcreteBlock {
 	}
 	
 	@Override
+	public int getExpDrop(IBlockAccess world, int metadata, int fortune) {
+		return 5;
+	}
+	
+	@Override
 	public int getMaxMeta() {
 		return 14;
 	}
-		
+				
 	@SideOnly(Side.CLIENT)
 	public IIcon getIcon(int side, int meta) {
 		return textures[meta];
@@ -44,6 +53,19 @@ public class BlockConcrete extends BlockMetaBase implements IConcreteBlock {
 		list.add(new ItemStack(item, 1, 4));
 		list.add(new ItemStack(item, 1, 9));
 		list.add(new ItemStack(item, 1, 14));
+	}
+	
+	@Override
+	public void randomDisplayTick(World world, int x, int y, int z,	Random rand) {
+		
+		int meta = world.getBlockMetadata(x, y, z);
+		
+		if (meta < 3) {
+			
+			for (int i = 0; i < (3 - meta); i++) {
+				world.spawnParticle("blockdust_" + Block.getIdFromBlock(this) + "_" + world.getBlockMetadata(x, y, z), x + rand.nextDouble(), y, z + rand.nextDouble(), 0, 0, 0);
+			}		
+		}
 	}
 	
 	@SideOnly(Side.CLIENT)
