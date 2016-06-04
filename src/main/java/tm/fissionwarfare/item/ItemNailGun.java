2150f66e928d948f078522a43ca1dadd713a647e
Grab Chips <thesteampunksteve@gmail.com>
2016-06-04 16:09:07 -0400

    spacing

--- src/main/java/tm/fissionwarfare/item/ItemNailGun.java
+++ src/main/java/tm/fissionwarfare/item/ItemNailGun.java
@@ -18,23 +18,28 @@ public class ItemNailGun extends ItemBase {
 	
 	@Override
 	public boolean onItemUse(ItemStack is, EntityPlayer player, World world, int x, int y, int z, int meta, float float1, float float2, float float3) {
-	double xd = (double) x;
-	double yd = (double) y;
-	double zd = (double) z;
+		
+		double xd = (double) x;
+		double yd = (double) y;
+		double zd = (double) z;
+		
 		if (world.getBlock(x, y, z) == InitBlocks.concrete){
 
-			if(player.inventory.hasItemStack(new ItemStack(InitItems.nail)) && player.inventory.hasItemStack(new ItemStack(InitItems.invar_frame))){
+			if (player.inventory.hasItemStack(new ItemStack(InitItems.nail)) && player.inventory.hasItemStack(new ItemStack(InitItems.invar_frame))){
+				
 				if (world.getBlockMetadata(x, y, z) < 14){
+					
 					player.inventory.consumeInventoryItem(InitItems.invar_frame);
 					player.inventory.consumeInventoryItem(InitItems.nail);
 					int get = world.getBlockMetadata(x, y, z);
 					world.setBlockMetadataWithNotify(x, y, z, get + 5, 2);
-					//world.playSound(xd, yd, zd, "anvil_land", 1.0F, 1.0F, false);
+					world.playSound(xd, yd, zd, "random.anvil_land", 1.0F, 1.0F, false);
 				}
 			}
-		}else{
-			System.out.println("NOT CONCRETE");
 		}
+		
+		else System.out.println("NOT CONCRETE");
+		
 		return true;
 	}
 
