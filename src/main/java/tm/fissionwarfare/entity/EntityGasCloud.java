7121b79d6ee1cf3f12103776ce66cadaa0bb9121
Grab Chips <thesteampunksteve@gmail.com>
2016-06-25 13:16:36 -0400

    Chemical fixed.

--- src/main/java/tm/fissionwarfare/entity/EntityGasCloud.java
+++ src/main/java/tm/fissionwarfare/entity/EntityGasCloud.java
@@ -15,10 +15,11 @@ public class EntityGasCloud extends Entity {
 
 	private static final double MAX_LIFE = 20 * 10;
 	private static final double DAMAGE = 3;	
-	private double range = 10;
+	private double range;
 	
 	public EntityGasCloud(World world) {
 		super(world);
+		setSize(0, 0);
 	}
 
 	public EntityGasCloud(World world, double x, double y, double z) {
@@ -32,8 +33,8 @@ public class EntityGasCloud extends Entity {
 	@Override
 	public void onUpdate() {		
 		super.onUpdate();
-		
-		range = ticksExisted * (0.001);
+				
+		range = 10 + (ticksExisted * (0.05));
 
 		if (ticksExisted >= MAX_LIFE) {
 			setDead();
@@ -48,6 +49,30 @@ public class EntityGasCloud extends Entity {
 		}
 	}
 
+	private void doDamage() {
+
+		CopyOnWriteArrayList<Object> list = new CopyOnWriteArrayList<Object>(worldObj.loadedEntityList);
+
+		for (Object obj : list) {
+
+			if (obj instanceof EntityLivingBase) {
+
+				EntityLivingBase living = (EntityLivingBase) obj;
+				
+				System.out.println(range);
+				
+				if (living.getDistance(posX, posY, posZ) <= range) {
+
+					living.attackEntityFrom(DamageSource.magic, (float) DAMAGE);
+					living.addPotionEffect(new PotionEffect(Potion.poison.id, 250, 0));
+					living.addPotionEffect(new PotionEffect(Potion.moveSlowdown.id, 300, 2));
+					living.addPotionEffect(new PotionEffect(Potion.confusion.id, 250, 0));
+					living.addPotionEffect(new PotionEffect(Potion.blindness.id, 300, 2));
+				}
+			}
+		}
+	}
+	
 	private void doEffects() {
 		
 		double x = posX + (rand.nextDouble() * (range * 2)) - range;
@@ -62,28 +87,6 @@ public class EntityGasCloud extends Entity {
 			
 			worldObj.spawnParticle("hugeexplosion", x, y, z, 0.0, 0.0, 0.0);
 			worldObj.spawnParticle("hugeexplosion", xx, yy, zz, 0.0, 0.0, 0.0);
-		}
-	}
-
-	private void doDamage() {
-
-		CopyOnWriteArrayList<Object> list = new CopyOnWriteArrayList<Object>(worldObj.loadedEntityList);
-
-		for (Object obj : list) {
-
-			if (obj instanceof EntityLivingBase) {
-
-				EntityLivingBase living = (EntityLivingBase) obj;
-
-				if (living.getDistance(posX, posY, posZ) <= range) {
-
-					living.attackEntityFrom(DamageSource.magic, (float) DAMAGE);
-					living.addPotionEffect(new PotionEffect(Potion.poison.id,250,0));
-					living.addPotionEffect(new PotionEffect(Potion.moveSlowdown.id,300,2));
-					living.addPotionEffect(new PotionEffect(Potion.confusion.id,250,0));
-					living.addPotionEffect(new PotionEffect(Potion.blindness.id,300,2));
-				}
-			}
 		}
 	}
 
